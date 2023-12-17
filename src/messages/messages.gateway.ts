1ee59b86511fc6f53245ac363b0b212514c6a877
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-18 01:21:27 +0700

    [Feat]: Add event emitter and handle join event in MessagesGateway

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -13,6 +13,8 @@ import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { Message } from './entities/message.entity';
 import { Server, Socket } from 'socket.io';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { NotifyChatRoomCreatedEvent } from 'src/notify/dto/notify-chat-room-created.event';
 
 @WebSocketGateway({
   cors: {
@@ -24,7 +26,10 @@ import { Server, Socket } from 'socket.io';
 export class MessagesGateway
   implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
 {
-  constructor(private readonly messagesService: MessagesService) {}
+  constructor(
+    private readonly messagesService: MessagesService,
+    private readonly eventEmitter: EventEmitter2,
+  ) {}
 
   @WebSocketServer()
   server: Server;
@@ -50,7 +55,7 @@ export class MessagesGateway
       createMessageDto,
       client.id,
     );
-    this.server.emit('message', message); // call the event 'message' and send the message on the all clients 
+    this.server.emit('message', message); // call the event 'message' and send the message on the all clients
     return message;
   }
 
@@ -60,10 +65,13 @@ export class MessagesGateway
   }
 
   @SubscribeMessage('join')
-  handleEventJoinRoom(
+  async handleEventJoinRoom(
     @MessageBody('name') name: string,
     @ConnectedSocket() client: Socket,
   ) {
+    this.eventEmitter.emit('user.joined', new NotifyChatRoomCreatedEvent(name, client.id)); // call the internal event 'user.joined' in the NotifyService
+    const result = await this.eventEmitter.emitAsync('ads.enabled', true); // call the internal event 'ads.enabled' in the AdsService
+    console.log(result); // [1, 2]
     return this.messagesService.identifyUser(name, client.id);
   }
 
