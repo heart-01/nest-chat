3fd6d6550acc8b5a8d58b42c985508cab2ec7e29
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-18 01:21:18 +0700

    [Feat]: Add NotifyModule and AdsModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
+import { NotifyModule } from './notify/notify.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { AdsModule } from './ads/ads.module';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [EventEmitterModule.forRoot(), MessagesModule, NotifyModule, AdsModule],
   controllers: [],
   providers: [],
 })
